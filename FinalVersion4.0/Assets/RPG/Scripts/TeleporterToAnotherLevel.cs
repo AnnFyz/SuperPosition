3c878401661b03a8bbe656242b9e4bdd5870fc1b
AnnFyz <annfyz1232@yandex.ru>
2022-08-24 01:57:58 +0200

    complete 1 level

---
+++ FinalVersion4.0/Assets/RPG/Scripts/TeleporterToAnotherLevel.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterToAnotherLevel : MonoBehaviour
+{
+
+    public int wantedKeyId;
+    public bool WasUsedRightKey = false;
+    public GameObject windowNoKey;
+    public GameObject windowHasKey;
+    public bool playerDetected;
+    public GameObject player;
+    public List<GameObject> allKeyArticles;
+    public Slot slot;
+    public int slotsIndex = 0;
+    public GameManager gameManager;
+    private void Start()
+    {
+        ToggleNoKey(false);
+        ToggleHasKey(false);
+        player = GameObject.FindGameObjectWithTag("Player");
+        allKeyArticles = new List<GameObject>(GameObject.FindGameObjectsWithTag("Slot"));
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private void ToggleNoKey(bool show)
+    {
+        windowNoKey.SetActive(show);
+    }
+    public void ToggleHasKey(bool show)
+    {
+        windowHasKey.SetActive(show);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerDetected = true;
+            if (!WasUsedRightKey)
+            {
+                ToggleNoKey(playerDetected);
+            }
+            if (WasUsedRightKey)
+            {
+                ToggleHasKey(playerDetected);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerDetected = false;
+
+            if (!WasUsedRightKey)
+            {
+                ToggleNoKey(playerDetected);
+            }
+            if (WasUsedRightKey)
+            {
+                ToggleHasKey(playerDetected);
+            }
+        }
+    }
+
+   
+
+    private void DestroyKeyArticle()
+    {
+
+        if (playerDetected)
+        {
+            for (int i = 0; i < allKeyArticles.Count; i++)
+            {
+                slot = allKeyArticles[i].GetComponent<Slot>();
+                slotsIndex++;
+
+                if (slot == null || slot.transform.childCount <= 0)
+                {
+                    continue;
+                }
+                if (slot.GetComponentInChildren<ItemUIDescription>().idKey == wantedKeyId)
+                {
+                    WasUsedRightKey = true;
+                    ToggleNoKey(false);
+                    slot.DropItem();
+
+                }
+
+            }
+        }
+
+
+    }
+
+    private void Update()
+    {
+        if (playerDetected && Slot.isChecking)
+        {
+            DestroyKeyArticle();
+        }
+
+
+        if (playerDetected && Input.GetKeyDown(KeyCode.E) && WasUsedRightKey)
+        {
+            gameManager.LoadScene();//Teleport to another level
+
+        }
+    }
+
+
+}
--- FinalVersion4.0/Assets/Scripts/GameManager.cs
+++ FinalVersion4.0/Assets/Scripts/GameManager.cs
@@ -6,15 +6,10 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int indexScene;
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene()
     {
-        
+        SceneManager.LoadScene(indexScene++);
     }
 }
